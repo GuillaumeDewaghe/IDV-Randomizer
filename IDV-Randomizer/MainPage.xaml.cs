85f0a9df0d3bb7b7c58ed30161bd18244d01f817
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2023-05-06 18:12:45 +0200

    Add first view of generation of random character

--- IDV-Randomizer/MainPage.xaml.cs
+++ IDV-Randomizer/MainPage.xaml.cs
@@ -1,24 +1,31 @@
-﻿namespace IDV_Randomizer;
+﻿using IDV_Randomizer.Models;
+
+namespace IDV_Randomizer;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private List<BaseCharacter> charactersList;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        BaseCharacter magician = new("Magician", "magician.png");
+        BaseCharacter ripper = new("Ripper", "ripper.png");
+        BaseCharacter disciple = new("Disciple", "disciple.png");
+        charactersList = new List<BaseCharacter>
+        {
+            magician,
+            ripper,
+            disciple
+        };
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void GenerateButton_Clicked(object sender, EventArgs e)
+    {
+        Random random = new Random();
+        var chosenCharacter = charactersList[random.Next(charactersList.Count)];
+        CharacterLabel.Text = chosenCharacter.GetName();
+        CharacterImage.Source = chosenCharacter.GetImageSource();
+    }
 }
 
