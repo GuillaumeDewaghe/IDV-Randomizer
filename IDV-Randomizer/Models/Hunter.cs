43f3bf613c933a35c0673fac4b5e1768cace65aa
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2023-05-07 00:45:20 +0200

    Create Hunter.cs

---
+++ IDV-Randomizer/Models/Hunter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDV_Randomizer.Models
+{
+    public sealed class Hunter : BaseCharacter
+    {
+        public enum Skill
+        {
+            Listen,
+            Abnormal,
+            Excitement,
+            Patroller,
+            Teleport,
+            Peeper,
+            Blink
+        }
+
+        private List<Skill> skillsList;
+
+        public Hunter(string name, ImageSource imageSource) : base(name, imageSource)
+        {
+            skillsList = new List<Skill>();
+            foreach (Skill skill in Enum.GetValues(typeof(Skill)))
+            {
+                skillsList.Add(skill);
+            }
+        }
+
+        public List<Skill> GetSkillsList()
+        {
+            return skillsList;
+        }
+    }
+}
