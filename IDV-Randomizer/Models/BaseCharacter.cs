fe96836521ac149677c3d4b5e87bf1cbeea7f411
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2023-05-06 18:10:18 +0200

    Create BaseCharacter class
    Add some pictures of characters for tests

---
+++ IDV-Randomizer/Models/BaseCharacter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDV_Randomizer.Models
+{
+    public class BaseCharacter
+    {
+        private string name;
+        private ImageSource imageSource;
+
+        public BaseCharacter(string name, ImageSource imageSource)
+        {
+            this.name = name;
+            this.imageSource = imageSource;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public ImageSource GetImageSource()
+        {
+            return imageSource;
+        }
+    }
+}
