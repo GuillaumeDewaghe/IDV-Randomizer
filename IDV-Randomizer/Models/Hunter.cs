b66612cfc6a66e94d9da901cf39f6b12f378218f
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2023-05-08 22:06:40 +0200

    Update Hunter.cs
    Change fields into properties
    
    Put Skill into a separate file

--- IDV-Randomizer/Models/Hunter.cs
+++ IDV-Randomizer/Models/Hunter.cs
@@ -7,33 +7,16 @@ using System.Threading.Tasks;
 namespace IDV_Randomizer.Models
 {
     public sealed class Hunter : BaseCharacter
-    {
-        public enum Skill
+    {      
+        public Skill Skill
         {
-            Listen,
-            Abnormal,
-            Excitement,
-            Patroller,
-            Teleport,
-            Peeper,
-            Blink
+            get;
+            set;
         }
-
-        private Skill skill;
 
         public Hunter(string name, ImageSource imageSource) : base(name, imageSource)
         {
-
-        }
-
-        public Skill GetSkill()
-        {
-            return skill;
-        }
-
-        public void SetSkill(Skill skill)
-        {
-            this.skill = skill;
+            
         }
     }
 }
---
+++ IDV-Randomizer/Models/Skill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDV_Randomizer.Models
+{
+    public enum Skill
+    {
+        Listen,
+        Abnormal,
+        Excitement,
+        Patroller,
+        Teleport,
+        Peeper,
+        Blink
+    }
+}
