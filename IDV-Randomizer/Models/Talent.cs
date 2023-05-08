83f7c5574d5cfb803696e9ffd2c30dc00558735b
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2023-05-08 22:43:25 +0200

    Create Talent.cs

---
+++ IDV-Randomizer/Models/Talent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDV_Randomizer.Models
+{
+    public sealed class Talent
+    {
+        public string Name { get; private set; }
+        public ImageSource ImageSource { get; private set; }
+        public int CurrentNbPoints { get; set; } = 0;
+        public int MaxPoints { get; private set; }
+
+        public Talent(string name, ImageSource imageSource, int maxPoints)
+        {
+            Name = name;
+            ImageSource = imageSource;
+            MaxPoints = maxPoints;
+        }
+    }
+}
