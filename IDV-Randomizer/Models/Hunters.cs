c764c2e261b0a71ab29fe19195bb73d1966617c0
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2023-06-22 19:31:28 +0200

    Create Hunters class

---
+++ IDV-Randomizer/Models/Hunters.cs
@@ -0,0 +1,34 @@
+﻿using IDV_Randomizer.Resources;
+
+namespace IDV_Randomizer.Models
+{
+    public static class Hunters
+    {
+        public static Hunter HellEmber { get; } = new Hunter(AppResources.HellEmberName, AppResources.HellEmberImage);
+        public static Hunter SmileyFace { get; } = new Hunter(AppResources.SmileyFaceName, AppResources.SmileyFaceImage);
+        public static Hunter Gamekeeper { get; } = new Hunter(AppResources.GamekeeperName, AppResources.GamekeeperImage);
+        public static Hunter Ripper { get; } = new Hunter(AppResources.RipperName, AppResources.RipperImage);
+        public static Hunter SloulWeaver { get; } = new Hunter(AppResources.SoulWeaverName, AppResources.SoulWeaverImage);
+        public static Hunter Geisha { get; } = new Hunter(AppResources.GeishaName, AppResources.GeishaImage);
+        public static Hunter WuChang { get; } = new Hunter(AppResources.WuChangName, AppResources.WuChangImage);
+        public static Hunter Photographer { get; } = new Hunter(AppResources.PhotographerName, AppResources.PhotographerImage);
+        public static Hunter MadEyes { get; } = new Hunter(AppResources.MadEyesName, AppResources.MadEyesImage);
+        public static Hunter Feaster { get; } = new Hunter(AppResources.FeasterName, AppResources.FeasterImage);
+        public static Hunter DreamWitch { get; } = new Hunter(AppResources.DreamWitchName, AppResources.DreamWitchImage);
+        public static Hunter AxeBoy { get; } = new Hunter(AppResources.AxeBoyName, AppResources.AxeBoyImage);
+        public static Hunter EvilReptilian { get; } = new Hunter(AppResources.EvilReptilianName, AppResources.EvilReptilianImage);
+        public static Hunter BloodyQueen { get; } = new Hunter(AppResources.BloodyQueenName, AppResources.BloodyQueenImage);
+        public static Hunter Guard26 { get; } = new Hunter(AppResources.Guard26Name, AppResources.Guard26Image);
+        public static Hunter Disciple { get; } = new Hunter(AppResources.DiscipleName, AppResources.DiscipleImage);
+        public static Hunter Violonist { get; } = new Hunter(AppResources.ViolonistName, AppResources.ViolonistImage);
+        public static Hunter Sculptor { get; } = new Hunter(AppResources.SculptorName, AppResources.SculptorImage);
+        public static Hunter Undead { get; } = new Hunter(AppResources.UndeadName, AppResources.UndeadImage);
+        public static Hunter BreakingWheel { get; } = new Hunter(AppResources.BreakingWheelName, AppResources.BreakingWheelImage);
+        public static Hunter Naid { get; } = new Hunter(AppResources.NaiadName, AppResources.NaiadImage);
+        public static Hunter WaxArtist { get; } = new Hunter(AppResources.WaxArtistName, AppResources.WaxArtistImage);
+        public static Hunter Nightmare { get; } = new Hunter(AppResources.NightmareName, AppResources.NightmareImage);
+        public static Hunter Clerk { get; } = new Hunter(AppResources.ClerkName, AppResources.ClerkImage);
+        public static Hunter Hermit { get; } = new Hunter(AppResources.HermitName, AppResources.HermitImage);
+        public static Hunter NightWatch { get; } = new Hunter(AppResources.NightWatchName, AppResources.NightWatchImage);
+    }
+}
